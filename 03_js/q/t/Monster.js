3cf0dbf9a1b5dd1674be73b68e9f3e269a6d6a78
KimDoGyuem <dkdiskal357@gmail.com>
2024-08-13 02:11:00 +0900

    Delete 03_js/q/t directory

--- 03_js/q/t/Monster.js
+++
@@ -1,9 +0,0 @@
-function Monster(name, hp, attack) {
-    this.name = name;
-    this.maxhp = hp;
-    this.curhp = hp;
-    this.attack = attack;
-    this.info = function () {
-        dw("[" + this.name + "]|HP(" + this.curhp + "/" + this.maxhp + ")|");
-    }
-}
